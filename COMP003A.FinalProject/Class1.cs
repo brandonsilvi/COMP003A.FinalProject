67bf57c234a3e65cf0a3eff9290d317626b72df8
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-03-03 08:27:45 -0800

    Working on fields, string completed for background info

--- COMP003A.FinalProject/Class1.cs
+++ COMP003A.FinalProject/Class1.cs
@@ -1,5 +1,24 @@
-﻿namespace COMP003A.FinalProject;
+﻿using System;
 
-public class Class1
+namespace COMP003A.FinalProject;
+
+public class IntakeRecord
 {
+    //String Fields
+    public string FirstName;
+    public string LastName;
+    public string PhoneNumber;
+    public string EmailAddress;
+    public string IntendedMajor;
+    public string BackupMajor;
+    public string AppliedTerm;
+    public string HighSchoolName;
+    public string Gender;
+    public string EnrollmentStatus;
+    public string StateResidency;
+
+    //Numeric Fields
+    //Selection based fields
+    //Bool Fields
+
 }
