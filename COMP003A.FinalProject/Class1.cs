fc724d9cffb55654b8fabed3e6d6e18dff29209d
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-03-03 09:24:36 -0800

    Fields Completed

--- COMP003A.FinalProject/Class1.cs
+++ COMP003A.FinalProject/Class1.cs
@@ -4,6 +4,8 @@ namespace COMP003A.FinalProject;
 
 public class IntakeRecord
 {
+    // 25 FIELDS----------------------------
+    
     //String Fields
     public string FirstName;
     public string LastName;
@@ -18,7 +20,26 @@ public class IntakeRecord
     public string StateResidency;
 
     //Numeric Fields
+    public int Age;
+    public int SATScore;
+    public int ACTScore;
+    public int GraduationYear;
+    public int TransferableCredits;
+    public int ApplicationID;
+    public double GPA;
+    public double FamilyIncome;
+    public double ScholarshipAmount;
+    
+
     //Selection based fields
+    public string ApplicationType; //Transfer, Freshman
+    public string AdmissionDecision; //computed from SAT/ACT and GPA
+    
     //Bool Fields
+    public bool IsVeteran;
+    public bool HousingNeeded;
+    public bool FirstGeneration;
+    
+    //
 
 }
